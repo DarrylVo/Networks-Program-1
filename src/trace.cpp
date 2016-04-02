43f237d4c151cb13021f8ac8578f03c4201532db
darryl <darryl@Satellite>
2016-04-01 19:51:35 -0700

    initial commit with everything figured out

---
+++ src/trace.cpp
@@ -0,0 +1,12 @@
+#include <pcap.h>
+#include <iostream>
+#include "checksum.h"
+#include "trace.hpp"
+using namespace std;
+
+
+int main() {
+
+	cout<< "aay lmao" << endl;
+	return 0;
+}
---
+++ src/trace.hpp
@@ -0,0 +1,15 @@
+/*
+ * trace.hpp
+ *
+ *  Created on: Apr 1, 2016
+ *      Author: darryl
+ */
+
+#ifndef SRC_TRACE_HPP_
+#define SRC_TRACE_HPP_
+
+
+
+
+
+#endif /* SRC_TRACE_HPP_ */
