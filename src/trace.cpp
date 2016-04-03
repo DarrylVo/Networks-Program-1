85e12a351854379b2236dc3e6616acf443a7c469
darryl <darryl@Satellite>
2016-04-02 18:08:27 -0700

    gonna change header structs

--- src/trace.cpp
+++ src/trace.cpp
@@ -3,54 +3,74 @@
 #include <stdio.h>
 #include <string.h>
 #include "checksum.h"
+#include <netinet/ether.h>
 #include "trace.hpp"
 using namespace std;
 
-void printethernet(ethernet *eheader) {
+int printethernet(ethernet *eheader) {
+	ether_addr dest;
+	ether_addr src;
+	int type;
+
+	memcpy(dest.ether_addr_octet,eheader->dest,6);
+	memcpy(src.ether_addr_octet,eheader->src,6);
+	type = eheader->type[0]+eheader->type[1];
 	cout<< "\tEthernet Header:"<<endl;
-	printf("\t\tDestination MAC: ");
-	for(int i = 0; i < 5; i ++) {
-		printf("%x:",*(eheader->dest+i));
+	printf("\t\tDest MAC: %s\n",ether_ntoa(&dest));
+	printf("\t\tSource Mac: %s\n",ether_ntoa(&src));
+	switch (type) {
+		case ARP_TYPE: 	printf("\t\tType: ARP\n\n");
+		return ARP_TYPE;
+		break;
 	}
-	printf("%x\n",*(eheader->dest+5));
-	printf("")
-	cout <<"/t/tType:"<<endl;
 }
 
 void getethernet(const u_char *pktdata, ethernet *eheader) {
 	memcpy(eheader->dest,pktdata,6);
+	memcpy(eheader->src,pktdata+6,6);
+	memcpy(eheader->type,pktdata+12,2);
+}
+
+void analyze(pcap_t *cap) {
+	pcap_pkthdr *genericheader;
+	const u_char *pktdata;
+	ethernet eheader;
+	int i = 1;
+	int type;
+
+	//while there are packets left, extract data and print
+	while(pcap_next_ex(cap,&genericheader, &pktdata )==1) {
+		printf("\nPacket number: %d Packet Len: %d\n\n",i++,genericheader->len);
+		getethernet(pktdata,&eheader);
+		type = printethernet(&eheader);
+		switch(type) {
+			case ARP_TYPE:
+
+			break;
+		}
+	}
+
 
 }
 
-
 int main(int argc, char **argv) {
+	pcap_t *cap;
+	char errbuff[PCAP_ERRBUF_SIZE];
+
+	//check arg number
 	if(argc!=2) {
 		cerr<< "error, please give one argument for the .pcap file " <<endl;
 		return -1;
 	}
-	pcap_t *cap;
-	char errbuff[PCAP_ERRBUF_SIZE];
 
-
-
+	//open pcap file
 	cap = pcap_open_offline(argv[1],errbuff);
-	if(cap == NULL)
+	if(cap == NULL) {
 		cerr<<errbuff <<endl;
-
-	pcap_pkthdr *genericheader;
-	const u_char *pktdata;
-	ethernet eheader;
-
-	int err = pcap_next_ex(cap,&genericheader, &pktdata );
-
-	getethernet(pktdata,&eheader);
-	printethernet(&eheader);
-
-
-
-
+		return -1;
+	}
+	analyze(cap);
 	pcap_close(cap);
-
 
 	return 0;
 }
--- src/trace.hpp
+++ src/trace.hpp
@@ -8,15 +8,27 @@
 #ifndef SRC_TRACE_HPP_
 #define SRC_TRACE_HPP_
 
+#define ARP_TYPE 0xe
+
 typedef struct ethernet {
 	unsigned char src[6];
 	unsigned char dest[6];
 	unsigned char type[2];
 };
 
+typedef struct arp {
+	unsigned char op[2];
+	unsigned char sendermac[6];
+	unsigned char senderip[4];
+	unsigned char targetmac[6];
+	unsigned char targetip[4];
+};
+
 void getethernet(const u_char *pktdata, ethernet *e);
 
-void printethernet(ethernet *e);
+int printethernet(ethernet *e);
+
+void analyze(pcap_t *cap);
 
 
 
